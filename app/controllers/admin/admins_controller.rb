4f8f2f9fbf97e7112307884f369a047dcbe87a18
thuanheo <nguyenthuan2551995@gmail.com>
2018-03-27 19:29:18 +0700

    user-login

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,2 +1,10 @@
 class Admin::AdminsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      redirect_to admin_categories_path
+    else
+      redirect_to admin_root_path
+    end
+  end
 end
