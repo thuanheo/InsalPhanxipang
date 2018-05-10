4d17d15816d91ae24fc4cd0e32d323b395e12ad6
Ky Phong <nguyenvankyphong@gmail.com>
2018-05-10 16:44:17 +0700

    view categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 data_category = ["Ao dai", "Vest", "Ao phong"]
-user = User.create( 
-    name:"insal", 
-    password:"12345678", 
-    email:"admin@insal.com",  
-    address:"hue", 
-    phone:"01664304688", 
-    personal_card: "CMND", 
+user = User.create(
+    name:"insal",
+    password:"12345678",
+    email:"admin@insal.com",
+    address:"hue",
+    phone:"01664304688",
+    personal_card: "CMND",
     role:"1")
 Category.create( name: "abc")
 bill = Bill.create(
-    start_renting: "27/09/2018", 
-    end_renting: "22/11/2018", 
-    total: "39.000", 
-    status:"hết hạn", 
-    user_id: user.id) 
+    start_renting: "27/09/2018",
+    end_renting: "22/11/2018",
+    total: "39.000",
+    status:"hết hạn",
+    user_id: user.id)
 data_category.each do |item|
     Category.create(name: item)
 end
 (1..200).to_a.each do |item|
-    Product.create( 
-        name: "hihi", 
-        price: item, 
-        renting_fee: "122", 
-        image:"ccccc", 
+    Product.create(
+        name: "hihi",
+        price: item,
+        renting_fee: "122",
+        image:"ccccc",
         category_id: 1)
   end
 (1..20).to_a.each do |item|
     Bill.create(
-        start_renting: "27/09/2018", 
-        end_renting: "22/11/2018", 
-        total: "39.000", 
-        status:"hết hạn", 
+        start_renting: "27/09/2018",
+        end_renting: "22/11/2018",
+        total: "39.000",
+        status:"hết hạn",
         user_id: 1)
 end
