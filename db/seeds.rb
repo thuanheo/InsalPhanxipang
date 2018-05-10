d414a04de5d503625f8d7dc6cd57293fc7dcc3c8
vovanthanh95 <vovanthanh180295@gmail.com>
2018-05-11 04:03:49 +0700

    fix-collapse

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-data_category = ["Ao dai", "Vest", "Ao phong"]
+//Create User
 user = User.create(
     name:"insal",
     password:"12345678",
@@ -7,25 +7,14 @@ user = User.create(
     phone:"01664304688",
     personal_card: "CMND",
     role:"1")
-Category.create( name: "abc")
 
-bill = Bill.create(
-    start_renting: "27/09/2018",
-    end_renting: "22/11/2018",
-    total: "39.000",
-    status:"hết hạn",
-    user_id: user.id)
+//Create category
+data_category = ["Ao dai", "Vest", "Ao phong"]
 data_category.each do |item|
     Category.create(name: item)
 end
-(1..200).to_a.each do |item|
-    Product.create(
-        name: "hihi",
-        price: item,
-        renting_fee: "122",
-        image:"ccccc",
-        category_id: 1)
-  end
+
+//Create bill
 (1..20).to_a.each do |item|
     Bill.create(
         start_renting: "27/09/2018",
@@ -34,8 +23,8 @@ end
         status:"hết hạn",
         user_id: 1)
  end
-Category.create( name: "haha")
-Category.create( name: "hihi")
+
+//Create bill
 bill = Bill.create(start_renting: "2016-10-05", end_renting: "", total: "1234", status: "0", user_id: user.id)
 r = Random.new
 arr = ["http://aocuoibangchau.vn/multidata/ao-cuoi-bang-chau-1.jpg",
@@ -45,5 +34,4 @@ arr = ["http://aocuoibangchau.vn/multidata/ao-cuoi-bang-chau-1.jpg",
    "http://www.marry.vn/wp-content/uploads/2017/09/15/r5.png"]
 (1..152).to_a.each do |item|
   Product.create(name: "name#{item}", price: item, renting_fee: "122", image: arr[r.rand(0...4)], category_id: r.rand(1..3))
-
 end
