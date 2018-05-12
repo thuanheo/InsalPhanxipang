438b299c58c0b58ad86de5aeeb3b40bb3b3bddd4
thuanheo <nguyenthuan2551995@gmail.com>
2018-05-12 17:46:52 +0700

    session

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,7 +1,9 @@
 class Admin::AdminsController < ApplicationController
+  before_action :login_success
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      log_in user
       redirect_to admin_categories_path, :notice => ["Đăng nhập thành công!", "success"]
     else
       redirect_to admin_root_path, :notice => ["Đăng nhập sai!", "error"]
